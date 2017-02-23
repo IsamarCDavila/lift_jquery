4238e7b3b4adaea4e9a6a3fcf4e118cd38f5ad0f
IsamarDavilaG <isamar.dvgz@gmail.com>
2017-02-23 14:48:25 -0500

    modificacion jquery

--- js/app.js
+++ js/app.js
@@ -1,67 +1,40 @@
-function init() {
+function init(){
     
 }
 
+var map;
 
-var miMapa;
 function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -16.457368, lng: -71.531508},
+    zoom: 16
+});
     
-    var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Laboratoria</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>laboratoria</b></p>'+
-      '</div>'+
-      '</div>';
-    
-    
-    
-    miMapa = new google.maps.Map(
-        document.getElementById('map'), 
-        {
-            center: {
-                lat: -16.457736, 
-                lng: -71.530440
-            },
-            zoom: 16
-        }
-    );
-    
-    var latLongPazPeru = {lat: -16.457736, lng: -71.530440};
-    var latDos = {lat: -16.456736, lng: -71.530140};
-    var latTres = {lat: -16.453736, lng: -71.530640};
-    var latCuatro = {lat: -16.451736, lng: -71.530940};
-    var latCinco = {lat: -16.459736, lng: -71.530340};
+    var latLongPazPeru = {lat:-16.457336,lng: -71.530440};
+    var latDos = {lat:-16.437368,lng: -71.551208};
+    var latTres = {lat:-16.457368,lng: -71.521508};
+    var latCuatro = {lat:-16.497368,lng: -71.511908};
     
     var pazPeru = new google.maps.Marker({
-    position: latLongPazPeru,
-    map: miMapa,
-    label: "l",
-    icon: 'image/carrin.png',
-    title: 'Aqui estamos!!!'
+        map: map,
+        position: latLongPazPeru,
+        title: 'Hello World!',
+        //label:'X',
+        icon: 'image/carriño.png'
     });
     
-    
-    new google.maps.Marker({position: latDos, map: miMapa,
-    label: "l",
-    icon: 'image/carrin.png'});
-    new google.maps.Marker({position: latTres, map: miMapa,
-    label: "l",
-    icon: 'image/carrin.png'});
-    new google.maps.Marker({position: latCuatro, map: miMapa,
-    label: "l",
-    icon: 'image/carrin.png'});
-    new google.maps.Marker({position: latCinco, map: miMapa,
-    label: "l",
-    icon: 'image/carrin.png'});
+    new google.maps.Marker({position:latDos, map:map, icon:'image/carriño.png'});
+    new google.maps.Marker({position:latTres, map:map, icon:'image/carriño.png'});
+    new google.maps.Marker({position:latCuatro, map:map, icon:'image/carriño.png'});
     
     var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
+        content:contentString
+    });
     
-    pazPeru.addListener('click', function() {
-    infowindow.open(map, pazPeru);
-  });
+    pazPeru.addEventListener('click', function(){
+        infowindow.open(map,pazPeru)
+    })
 }
+
+
 
--- js/signup.js
+++ js/signup.js
@@ -1,62 +1,94 @@
-var botonFlag = document.getElementById("btn-flag");
-botonFlag.addEventListener("click",onFlagClick);
+var nombre = $("#name");
+var emailX = $("#email");
+var checkInput = $("#check"); 
+var mensajeCheck = $("#mensajeCheck");
 
-function onFlagClick() {
-    window.location.href="paises.html";
+function init(){
+    var button = $("#next");
+    button.click(onButtonClick);
+    nombre.val("");
+    emailX.val("");
+    checkInput.prop( "checked" );
 }
-
-
-function init()
-{
-    var imgFlag = document.getElementById("img-flag");
-    
-    var select = localStorage.getItem('country_select'); 
-    var country_url = paises[select].imageURL;
-    
-    imgFlag.src=country_url;
-    
-    // ------------------------- CODIGO
-    var button = document.getElementById("next");
-    button.addEventListener('click',onButtonClick);
-}
-
-// --------------------------------------
-var inputNumber = document.getElementById("number");
-
 
 function onButtonClick() {
+    var inputName = $("#name");
+    localStorage.setItem('Name', inputName.val());
+    var inputEmail = $("#email");
+    localStorage.setItem('Email', inputEmail.val());
     
-    localStorage.setItem('Number', inputNumber.value);
-    
-    var mensajeNumber = document.getElementById("mensajeNumber");
-    
-    if(inputNumber.value == '') {
-        mensajeNumber.innerHTML = "<br><h5 class='text-center' style='color:#FF1493';>Debes escribir tu número telefónico</h5>"
+    if(nombre.val() == '' || emailX.val() == '' || checkInput.checked == false) {
+        
+        mensajeCheck.html("<br><h5 class='text-center' style='color:#FF1493';>Debe completar todo el formulario</h5>");
     } else {
-        mensajeNumber.innerHTML = '';
-        onCode();
-        location.href = 'signame.html';
+        
+        mensajeCheck.html("");
+        location.href = 'mapa.html';
+    }
+}
+//VALIDAR NOMBRE
+function validateName(_evt){
+    
+    var mensajeName = $("#mensajeName");
+    var textoNombre = "<h5 style='color:#FF1493';>Debe ingresar su nombre</h5>";
+    var textoNumero = "<h5 style='color:#FF1493';>Los números no son válidos</h5>";
+ 
+    
+    if(nombre.val() !=""){
+       
+        nombre.val(convertirMayus(nombre.val()));
+        
+        if(/([0-9])/g.test(nombre.val())){
+            mensajeName.html(textoNumero);
+        } else {
+            mensajeName.html("");
+        }
+    } else {
+        mensajeName.html(textoNombre);
+    }    
+}
+//VALIDAR EMAIL
+function validateEmail(_evt){
+    
+    var mensajeEmail = $("#mensajeEmail");
+    var textoEmail = "<h5 style='color:#FF1493';>Correo Electrónico Inválido, debe contener ejemplo: hola@dominio.com</h5>";
+
+    if(/([a-zA-Z0-9(-_.)]+[@][a-zA-Z0-9]+[.][a-zA-Z]+)/g.test(emailX.val())){
+        mensajeEmail.html(""); 
+    } else {
+        if(emailX.val().length >= 0){
+            mensajeEmail.html(textoEmail);   
+        }
     }
 }
 
-function onCode() {
-
-    var lab = "LAB-";
-    var aleatorio = Math.floor((Math.random()*999)+100);
-    var concat = (lab += aleatorio);
-    alert("Tu código de usuario es : " + concat);
-}
-
-
-function validateNumber(_evt){
+function convertirMayus(texto){
     
-    number = window.event.keyCode;
+    var nombreArray = texto.split("");
+    var primeraLetra = nombreArray[0];
+    var mayuscula = primeraLetra.toUpperCase();
+    var espacio = false;
 
+    for(var i=1; i<nombreArray.length; i++) {
 
-    if((number>=48 && number<=57)|| number==8 || number==32){
-            
+        if(espacio){
+            mayuscula += nombreArray[i].toUpperCase();
+            espacio = false;
+        } else {
+            mayuscula += nombreArray[i];
+            if(nombreArray[i] == " ")
+                espacio = true;
+        }
+    }
+    
+    return mayuscula;
+}
+    
+function onCheck(){   
+    
+    if(checkInput.checked){
+        return true;
     } else {
-        
-        _evt.preventDefault();
-    }     
+        return false;
+    }
 } 
